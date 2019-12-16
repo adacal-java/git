2719655c8a8d8c88978d8cb170a5003fd983c671
Eda Cal <edagi09@gmail.com>
2019-12-16 20:31:10 +0000

    last login.java

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -4,10 +4,10 @@ public class login {
     public static void main(String[] args) {
 
 // bu da yeni
-        
-        
 
-//bu da yesyeni from github....
+
+
+
         
       //xxxxxxxxxxxxxx from Intellij
 
